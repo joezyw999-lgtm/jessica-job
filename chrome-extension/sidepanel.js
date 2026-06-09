cdf18f6e29263ea597164c4b9061ace0dfef3922
User <user@example.com>
2026-06-09 15:43:54 +0800

    fix: 扩展 API 地址更新为当前沙箱域名

--- chrome-extension/sidepanel.js
+++ chrome-extension/sidepanel.js
@@ -1,5 +1,5 @@
 // ===== Config =====
-const DEFAULT_API = 'https://p-kwkmg4exknwx.cn-beijing.volces.com';
+const DEFAULT_API = 'https://b7e913e5-0d09-443d-a560-7d16316d211f.dev.coze.site';
 let API_BASE = DEFAULT_API;
 let deviceId = '';
 let pasteMode = 'single'; // single | multi
